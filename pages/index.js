385f1eeef36ab7393bdfd60c5aedbdea618104c8
ddv12138 <644077730@qq.com>
2024-12-09 14:36:58 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ ${urlHost || "egroup"} = select, policy-path=${convertedUrl}
               Proxy Provider Converter
             </h1>
             <p className="mt-2 md:text-lg text-gray-600">
-              一个可以将 Clash 订阅转换成 Proxy Provider 和 External
+              可以将 Clash 订阅转换成 Proxy Provider 和 External
               Group(Surge) 的工具
             </p>
           </div>
